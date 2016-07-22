51088b9289c33072ed3fc98f0669de39e2cfe043
Tim Martin <tim@timmartin.me>
2016-07-22 14:20:12 -0400

    Fixed tests

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    test_suite='tests',
+    test_suite='ultra_config_tests',
     tests_require=test_requirements
 )
