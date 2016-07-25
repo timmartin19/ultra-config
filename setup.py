c32eb223fc79e288dc75f1ce402906a730aa2181
Tim Martin <tim@timmartin.me>
2016-07-25 16:05:56 -0400

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='ultra_config',
-    version='0.1.6',
+    version='0.1.7',
     description="An extendable configuration that enables you to configure your application via python modules, config files, environment variables and more!",
     long_description=readme + '\n\n' + history,
     author="Tim Martin",
