d05d2b64b7c083d4fce923a375ac28a6359dda95
Tim Martin <tim@timmartin.me>
2016-07-26 16:14:51 -0400

    Import fix

--- ultra_config/ultra_config.py
+++ ultra_config/ultra_config.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 try:
     from configparser import ConfigParser
 except ImportError:
