aac5001de75f092afe7e314186e1cdc45d419da1
Tim Martin <tim@timmartin.me>
2016-08-02 21:42:23 -0400

    Bump version: 0.3.1 → 0.4.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='ultra_config',
-    version='0.3.1',
+    version='0.4.0',
     description="An extendable configuration that enables you to configure your application via python modules, config files, environment variables and more!",
     long_description=readme + '\n\n' + history,
     author="Tim Martin",
--- ultra_config.py
+++ ultra_config.py
@@ -14,7 +14,7 @@ from env_config import get_envvar_configuration
 
 __author__ = 'Tim Martin'
 __email__ = 'tim@timmartin.me'
-__version__ = '0.3.1'
+__version__ = '0.4.0'
 
 
 def load_python_module_settings(module):
