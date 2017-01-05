4552553ec10b9d71fb938388c25e74b6e34b2622
Tim Martin <tim@timmartin.me>
2017-01-05 13:23:25 -0500

    Bump version: 0.6.1 → 0.6.2

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ copyright = u"2016, Tim Martin"
 # the built documents.
 #
 # The short X.Y version.
-version = '0.6.1'
+version = '0.6.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
